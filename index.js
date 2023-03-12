fdc78efe196aa267f5b8eeb97f33e3940939cd0b
0xcryptolub <blackhat1523@gmail.com>
2023-03-12 18:58:25 +0530

    New and Updated html, js and css files are created with a.code

---
+++ index.js
@@ -0,0 +1,96 @@
+const avatarImg = document.querySelector('.avatar');
+var userName = document.querySelector('.nameInput');
+const button = document.querySelector('#button');
+const insp = document.querySelector('.up-ins');
+const instr = document.querySelector('.instr');
+let h1 = document.querySelector('h1');
+const refresh = document.querySelector('#refresh')
+const download = document.querySelector('#download')
+const copyr = document.querySelector('#copyr')
+const share = document.querySelector('#share')
+
+refresh.style.display='none'
+download.style.display='none'
+copyr.style.display='none'
+share.style.display='none'
+
+var inputName = "";
+var gender;
+
+var urlExtra = 'https://api.dicebear.com/5.x/adventurer/svg?size=200&scale=90&radius=50';
+var seed = '';
+var hair = '';
+var num;
+
+var url = "https://api.dicebear.com/5.x/adventurer/svg?size=200&scale=90&radius=50";
+
+const earring = parseInt(Math.random() * 6);
+// url += "&earrings=variant0" + earring + "&earringsProbability=100";
+// console.log(url);
+
+userName.addEventListener('keyup', () => {
+    inputName = userName.value;
+    // console.log(inputName)
+    var foundGender = "";
+    fetch('https://api.genderize.io/?name=' + inputName)
+    .then((res) => res.json())
+    .then((data) => {
+        foundGender = data.gender;
+        gender = foundGender;
+        if(inputName.length > 3){
+            changeAvatar();
+        }
+        // console.log(foundGender + gender);
+    })
+    // console.log(url);
+})
+// Change the avatar
+const changeAvatar = function(){
+    seed = '&seed=' + userName.value;
+    if(gender == 'male'){
+        num = parseInt(Math.random() * 19);
+        if(num==0) num=1;
+        hair = '&hair=short' + ("0" + num).slice(-2);
+    }
+    if(gender == 'female'){
+        num = parseInt(Math.random() * 26);
+        if(num == 0) num = 1;
+        hair = '&hair=long' + ("0" + num).slice(-2);
+    }
+    urlExtra = `${url}${seed}${hair}`
+    // console.log(urlExtra);
+    avatarImg.innerHTML = `
+    <img
+        src=${urlExtra}
+        alt="avatar"
+    />
+    `
+}
+
+//trying to add insult feature
+
+button.addEventListener('click', e =>{
+    e.preventDefault()
+    fetch('./insult.json')
+    .then(response => response.json())
+    .then(myObj => {
+        refresh.style.display='block'
+        download.style.display='block'
+        copyr.style.display='block'
+        share.style.display='block'
+        h1.innerText= userName.value
+        userName.style.display = 'none'
+        const randomIndex = Math.floor(Math.random() * myObj["insults"].length);
+        const randomInsult = myObj["insults"][randomIndex];
+        instr.innerHTML=""
+        // console.log(instr)
+        instr.innerText = randomInsult
+  });
+    button.style.display = 'none'
+    refresh.addEventListener('click', ()=>{
+        window.location.reload()
+    })
+})
+
+
+
